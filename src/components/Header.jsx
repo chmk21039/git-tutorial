e84e1f231902c0250c5d7f995324f974cd9dcf9e
chmk21039 <chmk21039@g.nihon-u.ac.jp>
2024-10-07 10:32:16 +0900

    コンポーネント作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Nav from "./Nav";
+export default function Header() {
+  return (
+    <header>
+      <h1>Birdwatching</h1>
+      <img src="../../webpro_03/assets/dove.png" alt="a simple dove logo"></img>
+      <Nav />
+    </header>
+  );
+}
