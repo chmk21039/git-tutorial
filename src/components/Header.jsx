e6f84a325761cfdd090c47aa724f4fd3ecd0ae90
chmk21039 <chmk21039@g.nihon-u.ac.jp>
2024-10-13 23:42:28 +0900

    画像パスの変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header>
       <h1>Birdwatching</h1>
-      <img src="../../public/assets/dove.png" alt="a simple dove logo"></img>
+      <img src="./public/assets/dove.png" alt="a simple dove logo"></img>
       <Nav />
     </header>
   );
